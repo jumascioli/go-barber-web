ad4f7a46be791b98f8af6ef51090486680cdfff6
EricDosReis <ericdosreis.personal@gmail.com>
2019-09-27 16:15:23 -0300

    Add reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
       }
     ],
     'import/prefer-default-export': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
